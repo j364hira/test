6829f36f77d8f566b5c6d54bdb62be6d228a3350
j364hira <yumayuma.h15@icloud.com>
2024-06-19 11:35:01 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("n = ");
+    scanf("%d", &n);
+
+    if ((n & 1) == 0)
+    {
+        printf("%d は偶数です\n", n);
+    }
+    else
+    {
+        printf("%d は奇数です\n", n);
+    }
+    return 0;
+}
